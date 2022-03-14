57b55dfa4d1884cc4d319841db8b51ba9b63d4a5
Fabio Lima <84404364+LimaDev-Max@users.noreply.github.com>
2022-03-14 16:18:20 -0300

    Create Aula01-Console e alertas.js

---
+++ Modulo-Logica de Progamação/Aula01-Console e alertas.js
@@ -0,0 +1,13 @@
+
+
+        console.log('olá Mundo 1');
+        console.log('olá Mundo 2');
+        console.log('olá Mundo 3');
+        console.log('olá Mundo 4');
+        console.log('olá Mundo 5');
+
+        window.alert("Olá mundo 1")
+        window.alert("Ola mundo 2")
+        window.alert("Ola mundo 3")
+
+    
