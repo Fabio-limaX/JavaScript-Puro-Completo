be12d8d6c9f50f2c54b20527a5ec391677f2197c
Fabio Lima <84404364+LimaDev-Max@users.noreply.github.com>
2022-03-14 16:48:34 -0300

    Update Aula04-Condições.js

---
+++ Modulo-Logica de Progamação/Aula04-Condições.js
@@ -0,0 +1,36 @@
+        // Condições\\
+
+    var numero = 10;
+    var nome = "Pedro";
+
+    if (nome == "Felipe"){
+        console.log("Verdadeiro");
+    }else if (nome == "Pedro"){
+        console.log("entramos no else if! Verdadeiro");
+    }else{
+        console.log("Entrou no else");
+    }
+
+        // Numeros\\
+
+    // >= Maior ou Igual
+    // > Maior
+    // <= Menor ou Igual
+    // < Menor
+
+    if(numero >= 10){
+        console.log("O número é Menor ou Igual a 10!");
+    }
+
+    //!= Diferente 
+     
+    if(nome != "Felipe"){
+        console.log("Enrou aqui! ")
+    }
+
+    // && 
+    // ||
+
+    if(numero == 10 && nome == "Pedro"){
+        console.log("Verdade");
+    }
